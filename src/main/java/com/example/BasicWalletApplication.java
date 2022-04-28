1e7ed3558a56539e4be1d5de0a00b52cae99899d
ibrahim.ozturk <ibrahim.ozturk@trendyol.com>
2022-04-28 17:46:42 +0300

    Add Snapshot

--- src/main/java/com/example/BasicWalletApplication.java
+++ src/main/java/com/example/BasicWalletApplication.java
@@ -1,16 +1,27 @@
 package com.example;
 
+import org.axonframework.eventsourcing.EventCountSnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.SnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.Snapshotter;
 import org.axonframework.springboot.autoconfig.AxonServerAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = AxonServerAutoConfiguration.class)
 public class BasicWalletApplication {
 
+    private static final int SNAPSHOT_THRESHOLD = 3;
+
     public static void main(String[] args) {
         SpringApplication.run(BasicWalletApplication.class, args);
+    }
+
+    @Bean
+    public SnapshotTriggerDefinition walletSnapshotDefinition(Snapshotter snapshotter) {
+        return new EventCountSnapshotTriggerDefinition(snapshotter, SNAPSHOT_THRESHOLD);
     }
 
 }
--- src/main/java/com/example/wallet/Wallet.java
+++ src/main/java/com/example/wallet/Wallet.java
@@ -14,7 +14,7 @@ import org.axonframework.spring.stereotype.Aggregate;
 
 import java.math.BigDecimal;
 
-@Aggregate
+@Aggregate(snapshotTriggerDefinition = "walletSnapshotDefinition")
 public class Wallet {
 
     @AggregateIdentifier
