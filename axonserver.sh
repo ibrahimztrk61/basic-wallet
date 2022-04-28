287a0a5b4ac7053ece18f0f63236bec4d6edd751
ibrahim.ozturk <ibrahim.ozturk@trendyol.com>
2022-04-28 15:51:32 +0300

    Add JPA support

--- axonserver.sh
+++
@@ -1,1 +0,0 @@
-docker run -p 8024:8024 -p 8124:8124 -e AXONIQ_AXONSERVER_DEVMODE_ENABLED=true -d axoniq/axonserver
--- src/main/java/com/example/BasicWalletApplication.java
+++ src/main/java/com/example/BasicWalletApplication.java
@@ -1,9 +1,12 @@
 package com.example;
 
+import org.axonframework.springboot.autoconfig.AxonServerAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = AxonServerAutoConfiguration.class)
 public class BasicWalletApplication {
 
     public static void main(String[] args) {
@@ -11,3 +14,4 @@ public class BasicWalletApplication {
     }
 
 }
+
