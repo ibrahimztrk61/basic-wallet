b6537022897b9a9d442e7d93f4675cc153977c4f
ibrahim.ozturk <ibrahim.ozturk@trendyol.com>
2022-05-05 03:22:11 +0300

    Add Amqp

--- src/main/java/com/example/BasicWalletApplication.java
+++ src/main/java/com/example/BasicWalletApplication.java
@@ -4,10 +4,17 @@ import org.axonframework.eventsourcing.EventCountSnapshotTriggerDefinition;
 import org.axonframework.eventsourcing.SnapshotTriggerDefinition;
 import org.axonframework.eventsourcing.Snapshotter;
 import org.axonframework.springboot.autoconfig.AxonServerAutoConfiguration;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = AxonServerAutoConfiguration.class)
@@ -22,6 +29,28 @@ public class BasicWalletApplication {
     @Bean
     public SnapshotTriggerDefinition walletSnapshotDefinition(Snapshotter snapshotter) {
         return new EventCountSnapshotTriggerDefinition(snapshotter, SNAPSHOT_THRESHOLD);
+    }
+
+    @EnableRabbit
+    @Configuration
+    public class RabbitConfig {
+
+        @Bean
+        public FanoutExchange exchange(@Value("${axon.amqp.exchange}") String name) {
+            FanoutExchange exchange = new FanoutExchange(name);
+            return exchange;
+        }
+
+        @Bean
+        public Queue queue() {
+            Queue queue = new Queue("wallet.events.queue");
+            return queue;
+        }
+
+        @Bean
+        public Binding binding(FanoutExchange exchange, Queue queue) {
+            return BindingBuilder.bind(queue).to(exchange);
+        }
     }
 
 }
